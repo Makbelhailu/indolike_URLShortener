32fbb131ca553321bbc823510220dbe6cbaf7730
Makbelhailu <mcwelson92@gmail.com>
2025-03-01 13:15:22 +0300

    creating the redirect url router and controller

--- controllers/url.js
+++ controllers/url.js
@@ -19,3 +19,16 @@ export async function generateShortUrl(req, res) {
     res.status(500).json({ error: err.message });
   }
 }
+
+export async function getOriginalUrl(req, res) {
+  try {
+    const { shortId } = req.params;
+    const url = await URL.findOne({ shortId });
+    if (!url) return res.status(404).json({ error: "URL not found" });
+    url.viewHistory.push({ timestamp: Date.now() });
+    await url.save();
+    res.redirect(url.redirectURL);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
--- routers/url.js
+++ routers/url.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { generateShortUrl } from "../controllers/url.js";
+import { generateShortUrl, getOriginalUrl } from "../controllers/url.js";
 
 const router = Router();
 
 router.post("/", generateShortUrl);
+router.get("/:shortId", getOriginalUrl);
 
 export default router;
